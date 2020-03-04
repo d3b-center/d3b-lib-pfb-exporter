53a899efdf6ca2dad3a9a553ba87ea233a2e7bf0
Natasha Singh <dukedesi22@gmail.com>
2020-03-03 23:14:46 -0500

    :white_check_mark: Test build PFB file

--- tests/test_pfbe.py
+++ tests/test_pfbe.py
@@ -1,10 +1,14 @@
 import os
+from collections import defaultdict
 from pprint import pprint
 
 from conftest import TEST_DATA_DIR
 from click.testing import CliRunner
+from fastavro import reader as avro_reader
+from deepdiff import DeepDiff
 
 from pfb_exporter import cli
+from pfb_exporter.builder import PfbFileBuilder
 
 OUTPUT_DIR = os.path.join(TEST_DATA_DIR, 'pfb_export')
 DATA_DIR = os.path.join(TEST_DATA_DIR, 'input')
@@ -35,3 +39,44 @@ def test_create_schema():
     )
 
     assert result.exit_code == 0
+
+
+def test_validate_pfb_build():
+    """
+    Validate the PFB file
+
+    Source data -> Write PFB File -> Read PFB file -> Compare with source data
+    """
+    # Create PFB file
+    builder = PfbFileBuilder(
+        DATA_DIR,
+        models_path=MODELS_PATH,
+        output_dir=OUTPUT_DIR
+    )
+    builder.build()
+
+    # Create pfb entities from source data
+    source_records = defaultdict(lambda: defaultdict(dict))
+    for record in builder.yield_entities():
+        source_records[record['name']][record['id']] = record
+
+    # Read PFB Entity JSON objects from PFB file
+    with open(builder.pfb_file, 'rb') as fo:
+        records_from_file = [record for record in avro_reader(fo)]
+
+    # Compare with original
+    for i, file_record in enumerate(records_from_file):
+        # if file_record['name'] == 'Metadata':
+        #     continue
+        print(f'Testing {file_record["name"]}')
+        source_record = source_records[file_record['name']][file_record['id']]
+        source_obj = source_record['object'][1]
+        file_obj = file_record['object']
+        diff = DeepDiff(source_obj, file_obj, ignore_order=True)
+        assert not diff
+
+    # Check output files
+    assert os.path.isfile(builder.model_builder.models_path)
+    assert os.path.isfile(builder.model_builder.orm_models_file)
+    assert os.path.isfile(builder.pfb_file_schema.avro_schema_file)
+    assert os.path.isfile(builder.metadata_file)
