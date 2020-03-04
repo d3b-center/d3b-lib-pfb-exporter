8a17951501ba44c02de9283ec58a63df7434f9f2
Natasha Singh <dukedesi22@gmail.com>
2020-03-04 00:24:43 -0500

    :construction_worker: Add build, test, clean scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Abort the script if there is a non-zero error
+set -e
+
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
+python setup.py install_egg_info
+pip install -e .
+pip install -r doc-requirements.txt
+
+cd docs
+doc8 source
+make html
+cd -
