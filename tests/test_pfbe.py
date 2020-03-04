6354b7831135abdf5d5eb2424c979f97ceafcf54
Natasha Singh <dukedesi22@gmail.com>
2020-03-03 22:50:38 -0500

    :white_check_mark: Minor refactor of tests to use model path

--- tests/test_pfbe.py
+++ tests/test_pfbe.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 
 from conftest import TEST_DATA_DIR
 from click.testing import CliRunner
@@ -7,18 +8,17 @@ from pfb_exporter import cli
 
 OUTPUT_DIR = os.path.join(TEST_DATA_DIR, 'pfb_export')
 DATA_DIR = os.path.join(TEST_DATA_DIR, 'input')
+MODELS_PATH = os.path.join(TEST_DATA_DIR, 'models.py')
 
 
 def test_export():
     """
     Test pfb_exporter.cli.export
     """
-    model_dir = DATA_DIR
-
     runner = CliRunner()
     result = runner.invoke(
         cli.export,
-        [DATA_DIR, '-m', model_dir, '-o', OUTPUT_DIR]
+        [DATA_DIR, '-m', MODELS_PATH, '-o', OUTPUT_DIR]
     )
 
     assert result.exit_code == 0
@@ -28,13 +28,10 @@ def test_create_schema():
     """
     Test pfb_exporter.cli.create_schema
     """
-    OUTPUT_DIR = os.path.join(TEST_DATA_DIR, 'pfb_export')
-    model_dir = DATA_DIR
-
     runner = CliRunner()
     result = runner.invoke(
         cli.create_schema,
-        [DATA_DIR, '-m', model_dir, '-o', OUTPUT_DIR]
+        [DATA_DIR, '-m', MODELS_PATH, '-o', OUTPUT_DIR]
     )
 
     assert result.exit_code == 0
